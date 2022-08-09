0871dbfc2cdb9b9f2c5785ebbd547e824aae9af6
Lauren Lee <86620096+laurenkiyomi@users.noreply.github.com>
2022-08-09 11:16:04 -0700

    Resolved dependencies

--- gohike-api/models/authorization.js
+++ gohike-api/models/authorization.js
@@ -71,6 +71,7 @@ class Authorization {
   static async loginUser(username, password) {
     // Log in user using Parse method
     const loginUser = await Parse.User.logIn(username, password);
+    
     return {
       sessionToken: loginUser.getSessionToken(),
       firstName: loginUser.get("firstName"),
